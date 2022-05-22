04fd8cbf8dd372a643f6fe7785ba6f910cb00a1f
Alain Espinosa <alainesp@gmail.com>
2022-05-22 16:48:23 +0200

    Only needed 'wy.hpp' for the library to function

--- tests.cpp
+++ tests.cpp
@@ -8,6 +8,48 @@
 // include the required header
 #include <wy.hpp>
 #include <gtest/gtest.h>
+#include "wyhash.h"
+
+/////////////////////////////////////////////////////////////////////////////////
+/// General
+/////////////////////////////////////////////////////////////////////////////////
+TEST(wyrand, Endianness)
+{
+	uint64_t val = 0x0102'0304'0506'0708;
+	uint8_t byteArray[8];
+
+	memcpy(byteArray, &val, sizeof(byteArray));
+
+	for (size_t i = 0; i < 8; i++)
+	{
+#if WYHASH_LITTLE_ENDIAN
+		ASSERT_EQ(byteArray[i], (val >> i * 8) & 0xff);
+#else
+		ASSERT_EQ(byteArray[7 - i], (val >> i * 8) & 0xff);
+#endif
+	}
+}
+
+#if !WYHASH_LITTLE_ENDIAN
+TEST(wyrand, Byteswap)
+{
+	uint64_t val64 = 0x0102'0304'0506'0708;
+	uint64_t swap64 = byteswap64(val64);	
+	for (size_t i = 0; i < 8; i++)
+	{
+		ASSERT_EQ((swap64 >> (7 - i) * 8) & 0xff, (val64 >> i * 8) & 0xff);
+	}
+	ASSERT_EQ(val64, byteswap64(swap64));
+
+	uint32_t val32 = 0x0102'0304;
+	uint32_t swap32 = byteswap32(val32);
+	for (size_t i = 0; i < 4; i++)
+	{
+		ASSERT_EQ((swap32 >> (3 - i) * 8) & 0xff, (val32 >> i * 8) & 0xff);
+	}
+	ASSERT_EQ(val32, byteswap32(swap32));
+}
+#endif
 
 /////////////////////////////////////////////////////////////////////////////////
 /// Pseudo-random number generation
--- wy.hpp
+++ wy.hpp
@@ -17,29 +17,143 @@
 #include <vector>
 #include <random>
 #include <string>
-#include "wyhash.h"
+#include <version>
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// wyhash.h
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef WYHASH_CONDOM
+	// protections that produce different results:
+	// 1: normal valid behavior
+	// 2: extra protection against entropy loss (probability=2^-63), aka. "blind multiplication"
+	#define WYHASH_CONDOM 1
+#endif
+
+#ifndef WYHASH_32BIT_MUM
+	// 0: normal version, slow on 32 bit systems
+	// 1: faster on 32 bit systems but produces different results, incompatible with wy2u0k function
+	#define WYHASH_32BIT_MUM 0  
+#endif
+
+// includes
+#if defined(_MSC_VER) && defined(_M_X64)
+	#include <intrin.h>
+	#pragma intrinsic(_umul128)
+#endif
+
+// likely and unlikely macros
+#if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+	#define _likely_(x)  __builtin_expect(x,1)
+	#define _unlikely_(x)  __builtin_expect(x,0)
+#else
+	#define _likely_(x) (x)
+	#define _unlikely_(x) (x)
+#endif
+
+// endian macros
+#ifndef WYHASH_LITTLE_ENDIAN
+	#if defined(_WIN32) || defined(__LITTLE_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+		#define WYHASH_LITTLE_ENDIAN 1
+	#elif defined(__BIG_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+		#define WYHASH_LITTLE_ENDIAN 0
+	#else
+		#warning could not determine endianness!Falling back to little endian.
+		#define WYHASH_LITTLE_ENDIAN 1
+	#endif
+#endif
 
 // Define 'byteswap64(uint64_t)' needed for 'rand::generate_stream(size_t)'
+// and 'byteswap32(uint32_t)' for other functions
 #if !WYHASH_LITTLE_ENDIAN
-#include <version>
-#ifdef __cpp_lib_byteswap
-#include <bit>
-#define byteswap64(v) std::byteswap(x)
-#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
-#define byteswap64(v) __builtin_bswap64(v)
-#elif defined(_MSC_VER)
-#define byteswap64(v) _byteswap_uint64(v)
-#else
-static inline uint64_t byteswap64(uint64_t v) noexcept
+	#ifdef __cpp_lib_byteswap
+		#include <bit>
+		#define byteswap64(v) std::byteswap(v)
+		#define byteswap32(v) std::byteswap(v)
+	#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+		#define byteswap64(v) __builtin_bswap64(v)
+		#define byteswap32(v) __builtin_bswap32(v)
+	#elif defined(_MSC_VER)
+		#define byteswap64(v) _byteswap_uint64(v)
+		#define byteswap32(v) _byteswap_ulong(v)
+	#else
+		static inline uint64_t byteswap64(uint64_t v) noexcept
+		{
+			v = ((v & 0x00000000FFFFFFFFull) << 32) | ((v & 0xFFFFFFFF00000000ull) >> 32);
+			v = ((v & 0x0000FFFF0000FFFFull) << 16) | ((v & 0xFFFF0000FFFF0000ull) >> 16);
+			v = ((v & 0x00FF00FF00FF00FFull) <<  8) | ((v & 0xFF00FF00FF00FF00ull) >>  8);
+			return v;
+		}
+		static inline uint32_t byteswap32(uint32_t v) noexcept
+		{
+			return (((v >> 24) & 0xff) | ((v >> 8) & 0xff00) | ((v << 8) & 0xff0000) | ((v << 24) & 0xff000000));
+		}
+	#endif
+#endif
+
+
+namespace wy::internal
 {
-	v = ((v & 0x00000000FFFFFFFFull) << 32) | ((v & 0xFFFFFFFF00000000ull) >> 32);
-	v = ((v & 0x0000FFFF0000FFFFull) << 16) | ((v & 0xFFFF0000FFFF0000ull) >> 16);
-	v = ((v & 0x00FF00FF00FF00FFull) << 8) | ((v & 0xFF00FF00FF00FF00ull) >> 8);
-	return v;
+	// 128bit multiply function
+#if(WYHASH_32BIT_MUM)
+	static inline uint64_t _wyrot(uint64_t x) { return (x >> 32) | (x << 32); }
+#endif
+	static inline void _wymum(uint64_t* A, uint64_t* B) {
+#if(WYHASH_32BIT_MUM)
+		uint64_t hh = (*A >> 32) * (*B >> 32), hl = (*A >> 32) * (uint32_t)*B, lh = (uint32_t)*A * (*B >> 32), ll = (uint64_t)(uint32_t)*A * (uint32_t)*B;
+	#if(WYHASH_CONDOM>1)
+		* A ^= _wyrot(hl) ^ hh; *B ^= _wyrot(lh) ^ ll;
+	#else
+		* A = _wyrot(hl) ^ hh; *B = _wyrot(lh) ^ ll;
+	#endif
+#elif defined(__SIZEOF_INT128__)
+		__uint128_t r = *A; r *= *B;
+	#if(WYHASH_CONDOM>1)
+		* A ^= (uint64_t)r; *B ^= (uint64_t)(r >> 64);
+	#else
+		* A = (uint64_t)r; *B = (uint64_t)(r >> 64);
+	#endif
+#elif defined(_MSC_VER) && defined(_M_X64)
+	#if(WYHASH_CONDOM>1)
+		uint64_t  a, b;
+		a = _umul128(*A, *B, &b);
+		*A ^= a;  *B ^= b;
+	#else
+		* A = _umul128(*A, *B, B);
+	#endif
+#else
+		uint64_t ha = *A >> 32, hb = *B >> 32, la = (uint32_t)*A, lb = (uint32_t)*B, hi, lo;
+		uint64_t rh = ha * hb, rm0 = ha * lb, rm1 = hb * la, rl = la * lb, t = rl + (rm0 << 32), c = t < rl;
+		lo = t + (rm1 << 32); c += lo < t; hi = rh + (rm0 >> 32) + (rm1 >> 32) + c;
+	#if(WYHASH_CONDOM>1)
+		* A ^= lo;  *B ^= hi;
+	#else
+		* A = lo;  *B = hi;
+	#endif
+#endif
+	}
+
+	// multiply and xor mix function, aka MUM
+	static inline uint64_t _wymix(uint64_t A, uint64_t B) { _wymum(&A, &B); return A ^ B; }
+
+	// read functions
+#if (WYHASH_LITTLE_ENDIAN)
+	static inline uint64_t _wyr8(const uint8_t* p) { uint64_t v; memcpy(&v, p, 8); return v; }
+	static inline uint64_t _wyr4(const uint8_t* p) { uint32_t v; memcpy(&v, p, 4); return v; }
+#else
+	static inline uint64_t _wyr8(const uint8_t* p) { uint64_t v; memcpy(&v, p, 8); return byteswap64(v); }
+	static inline uint64_t _wyr4(const uint8_t* p) { uint32_t v; memcpy(&v, p, 4); return byteswap32(v); }
+#endif
+	static inline uint64_t _wyr3(const uint8_t* p, size_t k) { return (((uint64_t)p[0]) << 16) | (((uint64_t)p[k >> 1]) << 8) | p[k - 1]; }
+
+	// The wyrand PRNG that pass BigCrush and PractRand
+	static inline uint64_t wyrand(uint64_t* seed) { *seed += 0xa0761d6478bd642full; return _wymix(*seed, *seed ^ 0xe7037ed1a0b428dbull); }
+
+#if(!WYHASH_32BIT_MUM)
+	// fast range integer random number generation on [0,k) credit to Daniel Lemire. May not work when WYHASH_32BIT_MUM=1. It can be combined with wyrand, wyhash64 or wyhash.
+	static inline uint64_t wy2u0k(uint64_t r, uint64_t k) { _wymum(&r, &k); return k; }
+#endif
 }
-#endif
-#endif
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 namespace wy {
@@ -72,7 +186,7 @@ namespace wy {
 		/// <returns>A new 64-bit pseudo-random value</returns>
 		inline uint64_t operator()() noexcept
 		{
-			return wyrand(&state);
+			return internal::wyrand(&state);
 		}
 		////////////////////////////////////////////////////////////////////////////////////
 
@@ -98,7 +212,10 @@ namespace wy {
 		/// <returns>The random value</returns>
 		inline double uniform_dist() noexcept
 		{
-			return wy2u01(operator()());
+			uint64_t r = operator()();
+			// code taken from 'wyhash.h::wy2u01(uint64_t r)'
+			constexpr double _wynorm = 1.0 / (1ull << 52);
+			return (r >> 12) * _wynorm;
 		}
 
 		/// <summary>
@@ -121,7 +238,7 @@ namespace wy {
 		/// <returns>The random value</returns>
 		inline uint64_t uniform_dist(uint64_t max_value) noexcept
 		{
-			return wy2u0k(operator()(), max_value);
+			return internal::wy2u0k(operator()(), max_value);
 		}
 
 		/// <summary>
@@ -130,7 +247,10 @@ namespace wy {
 		/// <returns>The random value</returns>
 		inline double gaussian_dist() noexcept
 		{
-			return wy2gau(operator()());
+			uint64_t r = operator()();
+			// code taken from 'wyhash.h::wy2gau(uint64_t r)'
+			constexpr double _wynorm = 1.0 / (1ull << 20);
+			return ((r & 0x1fffff) + ((r >> 21) & 0x1fffff) + ((r >> 42) & 0x1fffff)) * _wynorm - 3.0;
 		}
 
 		/// <summary>
@@ -203,7 +323,7 @@ namespace wy {
 			/// <summary>
 			/// Create a wyhasher with default secret
 			/// </summary>
-			hash_imp() noexcept : secret{ 0xa0761d6478bd642full, 0xe7037ed1a0b428dbull, 0x8ebc6af09c88c6e3ull, 0x589965cc75374cc3ull }
+			hash_imp() noexcept : secret{ 0xa0761d6478bd642full, 0xe7037ed1a0b428dbull, 0x8ebc6af09c88c6e3ull, 0x589965cc75374cc3ull }// the default secret parameters
 			{}
 			/// <summary>
 			/// Create a wyhasher with secret generated from a seed
@@ -211,7 +331,32 @@ namespace wy {
 			/// <param name="seed">The seed to generate the secret from</param>
 			hash_imp(uint64_t seed) noexcept
 			{
-				make_secret(seed, secret);
+				// make your own secret
+				// code taken from 'wyhash.h::make_secret(seed, secret)'
+				uint8_t c[] = { 15, 23, 27, 29, 30, 39, 43, 45, 46, 51, 53, 54, 57, 58, 60, 71, 75, 77, 78, 83, 85, 86, 89, 90, 92, 99, 101, 102, 105, 106, 108, 113, 114, 116, 120, 135, 139, 141, 142, 147, 149, 150, 153, 154, 156, 163, 165, 166, 169, 170, 172, 177, 178, 180, 184, 195, 197, 198, 201, 202, 204, 209, 210, 212, 216, 225, 226, 228, 232, 240 };
+				for (size_t i = 0; i < 4; i++) {
+					uint8_t ok;
+					do {
+						ok = 1; secret[i] = 0;
+						for (size_t j = 0; j < 64; j += 8) secret[i] |= ((uint64_t)c[wyrand(&seed) % sizeof(c)]) << j;
+						if (secret[i] % 2 == 0) { ok = 0; continue; }
+						for (size_t j = 0; j < i; j++) {
+#if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+							if (__builtin_popcountll(secret[j] ^ secret[i]) != 32) { ok = 0; break; }
+#elif defined(_MSC_VER) && defined(_M_X64)
+							if (_mm_popcnt_u64(secret[j] ^ secret[i]) != 32) { ok = 0; break; }
+#else
+							// manual popcount
+							uint64_t x = secret[j] ^ secret[i];
+							x -= (x >> 1) & 0x5555555555555555;
+							x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333);
+							x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f;
+							x = (x * 0x0101010101010101) >> 56;
+							if (x != 32) { ok = 0; break; }
+#endif
+						}
+					} while (!ok);
+				}
 			}
 			/// <summary>
 			/// Create a wyhasher with a specific secret
@@ -230,7 +375,31 @@ namespace wy {
 			/// <returns>A 64-bits hash</returns>
 			inline uint64_t wyhash(const uint8_t* data, size_t len) const noexcept
 			{
-				return ::wyhash(data, len, 0, secret);
+				// code taken from 'wyhash.h::wyhash(const void* key, size_t len, uint64_t seed, const uint64_t * secret)' with seed=0
+				const uint8_t* p = (const uint8_t*)data;
+				uint64_t seed = secret[0], a, b;
+				if (_likely_(len <= 16)) {
+					if (_likely_(len >= 4)) { a = (_wyr4(p) << 32) | _wyr4(p + ((len >> 3) << 2)); b = (_wyr4(p + len - 4) << 32) | _wyr4(p + len - 4 - ((len >> 3) << 2)); }
+					else if (_likely_(len > 0)) { a = _wyr3(p, len); b = 0; }
+					else a = b = 0;
+				}
+				else {
+					size_t i = len;
+					if (_unlikely_(i > 48)) {
+						uint64_t see1 = seed, see2 = seed;
+						do {
+							seed = _wymix(_wyr8(p) ^ secret[1], _wyr8(p + 8) ^ seed);
+							see1 = _wymix(_wyr8(p + 16) ^ secret[2], _wyr8(p + 24) ^ see1);
+							see2 = _wymix(_wyr8(p + 32) ^ secret[3], _wyr8(p + 40) ^ see2);
+							p += 48; i -= 48;
+						} while (_likely_(i > 48));
+						seed ^= see1 ^ see2;
+					}
+					while (_unlikely_(i > 16)) { seed = _wymix(_wyr8(p) ^ secret[1], _wyr8(p + 8) ^ seed);  i -= 16; p += 16; }
+					a = _wyr8(p + i - 16);  b = _wyr8(p + i - 8);
+				}
+				return _wymix(secret[1] ^ len, _wymix(a ^ secret[1], b ^ seed));
+
 			}
 			/// <summary>
 			/// Hash a 64-bit number
@@ -239,7 +408,12 @@ namespace wy {
 			/// <returns>A 64-bits hash</returns>
 			inline uint64_t wyhash(uint64_t number) const noexcept
 			{
-				return ::wyhash64(number, secret[0]);
+				// code taken from 'wyhash.h::wyhash64(uint64_t A, uint64_t B)'
+				uint64_t A = number;
+				uint64_t B = secret[0];
+				A ^= 0xa0761d6478bd642full; B ^= 0xe7037ed1a0b428dbull;
+				_wymum(&A, &B);
+				return _wymix(A ^ 0xa0761d6478bd642full, B ^ 0xe7037ed1a0b428dbull);
 			}
 		};
 
@@ -369,6 +543,7 @@ namespace wy {
 		using hash_string_base::hash_string_base;// Inherit constructors
 	};
 #endif
+
 	// std::pmr::string variants
 #if __cpp_lib_polymorphic_allocator
 	template<> struct hash<std::pmr::string> : public internal::hash_string_base<std::pmr::string>
